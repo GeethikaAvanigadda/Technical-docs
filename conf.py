89a4bdf1559e3f6beab4be32361eda132b5a41b5
Geethika Avanigadda <79193425+GeethikaAvanigadda@users.noreply.github.com>
2021-02-18 16:51:09 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'E-commerce project using Java'
-copyright = '2020, Harideep'
-author = 'Harideep'
+project = ' project using Java'
+copyright = '2020, Geethika'
+author = 'Geethika'
 
 # The full version, including alpha/beta/rc tags
 release = ''
